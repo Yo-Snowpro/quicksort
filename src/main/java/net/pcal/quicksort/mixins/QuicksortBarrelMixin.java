a8f82b2e3379111972501eece6d1b0125f1221ac
Yo-Snowpro <AidenMartinez.bfdi@gmail.com>
2024-08-17 14:12:37 -0400

    Update and rename QuicksortHopperMixin.java to QuicksortBarrelMixin.java

--- src/main/java/net/pcal/quicksort/mixins/QuicksortBarrelMixin.java
+++ src/main/java/net/pcal/quicksort/mixins/QuicksortBarrelMixin.java
@@ -1,7 +1,7 @@
 package net.pcal.quicksort.mixins;
 
 import net.minecraft.block.entity.ChestBlockEntity;
-import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.block.entity.BarrelBlockEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Direction;
@@ -14,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.stream.IntStream;
 
-@Mixin(HopperBlockEntity.class)
-public class QuicksortHopperMixin {
+@Mixin(BarrelBlockEntity.class)
+public class QuicksortBarrelMixin {
 
     @Inject(method = "transfer", at = @At("TAIL"))
     private static ItemStack transfer(@Nullable Inventory from,
@@ -25,7 +25,7 @@ public class QuicksortHopperMixin {
                                       CallbackInfoReturnable<ItemStack> callbackInfoReturnable) {
         final var result = callbackInfoReturnable.getReturnValue();
 
-        if (from instanceof final HopperBlockEntity hopperBlock && to instanceof ChestBlockEntity chest) {
+        if (from instanceof final BarrelBlockEntity barrelBlock && to instanceof ChestBlockEntity chest) {
             if (chest.getWorld() == null) {
                 return result;
             }
@@ -34,9 +34,9 @@ public class QuicksortHopperMixin {
                 chest.getWorld(), chest.getPos()
             ) > 0;
 
-            final var isHopperEmpty = IntStream.range(0, HopperBlockEntity.INVENTORY_SIZE).boxed()
-                                               .allMatch(hopperSlot -> hopperBlock.getStack(hopperSlot).isEmpty());
-            if (!isChestOpen && isHopperEmpty) {
+            final var isBarrelEmpty = IntStream.range(0, BarrelBlockEntity.INVENTORY_SIZE).boxed()
+                                               .allMatch(barrelSlot -> barrelBlock.getStack(barrelSlot).isEmpty());
+            if (!isChestOpen && isBarrelEmpty) {
                 // remove the old QuicksorterJob to avoid zombie jobs
                 QuicksortService.getInstance().onChestOpened(chest);
 
